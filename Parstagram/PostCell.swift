7146e9dcd40795537f965a4b08807c78348cbfa4
jasonbodzy <jasonbodzy@gmail.com>
2022-02-20 01:03:35 -0500

    internal bug fixes

---
+++ Parstagram/PostCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostCell.swift
+//  Parstagram
+//
+//  Created by Jason Bodzy on 2/20/22.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
