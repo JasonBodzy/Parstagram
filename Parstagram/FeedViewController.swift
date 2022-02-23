d1b6ac0091cca2de18ba522dd8ca9314890b7c14
jasonbodzy <jasonbodzy@gmail.com>
2022-02-23 16:51:50 -0500

    User can log out

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -77,6 +77,16 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     
 
     @IBOutlet weak var tableView: UITableView!
+    
+    @IBAction func onLogout(_ sender: Any) {
+        PFUser.logOut()
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
+        
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let delegate = windowScene.delegate as? SceneDelegate else {return}
+        
+        delegate.window?.rootViewController = loginViewController
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
--- Parstagram/LoginViewController.swift
+++ Parstagram/LoginViewController.swift
@@ -44,10 +44,10 @@ class LoginViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        initializeHideKeyboard()
         // Do any additional setup after loading the view.
     }
-    
+
 
     /*
     // MARK: - Navigation
@@ -59,4 +59,18 @@ class LoginViewController: UIViewController {
     }
     */
 
+}
+
+extension LoginViewController {
+    func initializeHideKeyboard(){
+        //Declare a Tap Gesture Recognizer which will trigger our dismissMyKeyboard() function
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissMyKeyboard))
+        //Add this tap gesture recognizer to the parent view
+        view.addGestureRecognizer(tap)
+    }
+    @objc func dismissMyKeyboard(){
+        //endEditing causes the view (or one of its embedded text fields) to resign the first responder status.
+        //In short- Dismiss the active keyboard.
+        view.endEditing(true)
+    }
 }
